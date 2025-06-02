b5fa8dc9c7d193fb515ff8a673cd2d418cecac3d
Jacob-T-Gardner <julzhu27@bergen.org>
2025-06-02 12:27:51 -0400

    it works but i still gotta change smtn x2

--- src/main/java/global/protocol/ClientJoinMessage.java
+++ src/main/java/global/protocol/ClientJoinMessage.java
@@ -6,4 +6,8 @@ public class ClientJoinMessage extends Message {
         this.username = username;
     }
 
+    @Override
+    public String toString() {
+        return "Welcome " + username + "!\n";
+    }
 }
